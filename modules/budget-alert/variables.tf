4b0b44522475e544f6ca7babce176616e2abbeaa
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-05-12 17:31:49 +0530

    some modifications

--- modules/budget-alert/variables.tf
+++ modules/budget-alert/variables.tf
@@ -3,4 +3,4 @@ variable "project_id" { type = string }
 variable "notification_channel_id" { type = string }
 variable "budget_limit" { type = string }
 variable "thresholds" { type = string }
-variable "enable_budget_alert" { type = bool }
+variable "enable_budget_alert" { type = bool }                          
